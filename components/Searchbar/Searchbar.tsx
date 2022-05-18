8ecfc9b553d4c7e9fbd6917cf094e7d6f703602b
skiran017 <skiran017@gmail.com>
2022-05-18 16:19:02 +0530

    feat: add search dropdown and show top traded pairs

--- components/Searchbar/Searchbar.tsx
+++ components/Searchbar/Searchbar.tsx
@@ -1,6 +1,14 @@
 import React from 'react';
 import Image from 'next/image';
-import { Flex, Box, Input } from '@chakra-ui/react';
+import { Flex, Box, Input, Text, chakra } from '@chakra-ui/react';
+import { topTradedPairs } from '../../utils/top5Pairs';
+
+interface Pair {
+  id: string;
+  ticker: string;
+  url: string;
+  description: string;
+}
 
 function Searchbar() {
   return (
@@ -31,6 +39,69 @@ function Searchbar() {
                 border="none"
                 _focus={{ boxShadow: 'none' }}
               />
+            </Flex>
+          </Box>
+          <Box
+            position="absolute"
+            inset="0px auto auto 0px"
+            maxW="450px"
+            transform="translate3d(541.935px, 78.2258px, 0px)"
+            w="100%"
+            zIndex="20"
+          >
+            <Flex
+              bgColor="white"
+              borderRadius="14px"
+              boxShadow={
+                '0 0 #0000,0 0 #0000,0 0 #0000,0 0 #0000,0 10px 15px -3px rgba(0,0,0,.1),0 4px 6px -4px rgba(0,0,0,.1)'
+              }
+              maxH="90vh"
+              height="100%"
+              width="100%"
+              direction="column"
+              // background="transparent"
+            >
+              <Flex
+                alignItems="center"
+                padding="6px 20px"
+                borderBottom="1px solid #eee"
+              >
+                <Flex alignItems="center">
+                  <Image
+                    src="/assets/icons/search.svg"
+                    width={18}
+                    height={18}
+                    alt="search"
+                  />
+                </Flex>
+                <Input
+                  placeholder="You can try 10 NEAR to USDC"
+                  border="none"
+                  _focus={{ boxShadow: 'none' }}
+                />
+              </Flex>
+              <Box maxH="50vh" color="black" overflowY="scroll">
+                <Text lineHeight="1.5" paddingX="20px" paddingTop="16px">
+                  Top Traded Pairs
+                </Text>
+                {topTradedPairs.map((pair: Pair) => {
+                  return (
+                    <chakra.a href={pair.url} key={pair.id}>
+                      <Box _hover={{ bgColor: '#eee' }} padding="14px 48px">
+                        <Flex>
+                          <Flex>
+                            <Flex direction="column">
+                              <Text>{pair.ticker}</Text>
+                              <Text fontSize="12px">{pair.description}</Text>
+                            </Flex>
+                          </Flex>
+                        </Flex>
+                      </Box>
+                      <Text></Text>
+                    </chakra.a>
+                  );
+                })}
+              </Box>
             </Flex>
           </Box>
         </Box>
