be860415f603415ac9cdb9681e44b37eb117cf07
skiran017 <skiran017@gmail.com>
2022-05-18 14:01:46 +0530

    feat: add search icon in search bar

--- components/Searchbar/Searchbar.tsx
+++ components/Searchbar/Searchbar.tsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import Image from 'next/image';
 import { Flex, Box, Input } from '@chakra-ui/react';
 
 function Searchbar() {
@@ -10,14 +11,27 @@ function Searchbar() {
             w="100%"
             borderRadius="14px"
             bgColor="white"
-            padding="14px"
+            padding="6px 20px"
             marginTop="8px"
+            boxShadow={
+              '0 10px 15px -3px rgba(0,0,0,.1),0 4px 6px -4px rgba(0,0,0,.1)'
+            }
           >
-            <Input
-              placeholder="You can try 10 NEAR to USDC"
-              border="none"
-              _focus={{ boxShadow: 'none' }}
-            />
+            <Flex alignItems="center">
+              <Flex alignItems="center">
+                <Image
+                  src="/assets/icons/search.svg"
+                  width={18}
+                  height={18}
+                  alt="search"
+                />
+              </Flex>
+              <Input
+                placeholder="You can try 10 NEAR to USDC"
+                border="none"
+                _focus={{ boxShadow: 'none' }}
+              />
+            </Flex>
           </Box>
         </Box>
       </Box>
