5e3267becd57716be2d2d73bfa4644b78bf83cfd
skiran017 <skiran017@gmail.com>
2022-05-18 09:14:39 +0530

    feat: add footer component

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,11 +1,13 @@
 import '../styles/globals.css';
 import type { AppProps } from 'next/app';
 import { ChakraProvider } from '@chakra-ui/react';
+import Footer from '../components/Footer/Footer';
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
     <ChakraProvider>
       <Component {...pageProps} />
+      <Footer />
     </ChakraProvider>
   );
 }
